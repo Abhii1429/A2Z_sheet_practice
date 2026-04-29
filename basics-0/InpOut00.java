ad3361e0c4d80026eca2d4496b680c51a614f183
Abhishek Pandey <hopesaliveabhi2003@gmail.com>
2026-04-29 10:15:39 +0530

    intial sheet que upload

---
+++ basics-0/InpOut00.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    Scanner sc = new Scanner(System.in);
+    
+    public void printNumber(Scanner sc) {
+        int n = sc.nextInt();
+     System.out.println(n);
+    }
+}
