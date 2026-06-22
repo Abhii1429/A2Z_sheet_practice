f4571aaa5f0fc43d3d3d775bfcaaf0dee58d7d8e
Abhishek Pandey <hopesaliveabhi2003@gmail.com>
2026-06-22 20:36:19 +0530

    rectangle

---
+++ basics-0/patterns/pattern1.java
@@ -0,0 +1,17 @@
+
+package patterns;
+class Solution {
+    public void pattern1(int n) {
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print("*");
+                
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.pattern1(4);
+    }
+}
