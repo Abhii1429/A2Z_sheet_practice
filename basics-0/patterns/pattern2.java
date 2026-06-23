f754fef6a4cba3b0cc8bc4a806937f3148efbca2
Abhishek Pandey <hopesaliveabhi2003@gmail.com>
2026-06-23 17:08:53 +0530

    triangle

--- basics-0/patterns/pattern2.java
+++ basics-0/patterns/pattern2.java
@@ -2,7 +2,7 @@ package patterns;
 class Pattern2 {
     public void pattern2(int n) {
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < i; j++){
+            for(int j = 0; j < i; j++){ // triangle
                 System.out.print("*");
                 
             }
