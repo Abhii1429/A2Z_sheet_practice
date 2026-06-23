0b2ff4e8726cf1804d6e5b7e3e209a32d1b3c8ba
Abhishek Pandey <hopesaliveabhi2003@gmail.com>
2026-06-23 17:09:19 +0530

    rectangle

--- basics-0/patterns/pattern1.java
+++ basics-0/patterns/pattern1.java
@@ -3,7 +3,7 @@ package patterns;
 class Solution {
     public void pattern1(int n) {
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+            for(int j = 0; j < n; j++){ // rectangle
                 System.out.print("*");
                 
             }
