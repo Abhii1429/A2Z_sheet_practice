9825aa106ecd32de4c7901f7910172f7faeb28a3
Abhishek Pandey <hopesaliveabhi2003@gmail.com>
2026-08-11 20:09:04 +0530

    Diamond patt

---
+++ basics-0/patterns/Pattern8.java
@@ -0,0 +1,49 @@
+package patterns;
+
+public class Pattern8 {  // Diamond pattern
+   public void pat8(int n){
+
+    // pyramid
+    for(int i = 0; i < n; i++){
+        // spaces
+        for(int j = 0; j < n-i-1; j++){ // j = n-i when  starts with 1
+            System.out.print(" ");
+        }
+
+        // stars
+        for(int j = 0; j < 2*i+1; j++){
+            System.out.print("*");
+        }
+
+        // spaces
+        for(int j = 0; j < n-i-1; j++){ // j = n-i when  starts with 1
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+//----------------------------------------------------
+    // inverted pyramid
+    for(int i = 0; i < n; i++){
+        // spaces
+        for(int j = 0; j < i; j++){
+            System.out.print(" ");
+        }
+        
+        // stars
+        for(int j = 0; j < 2*n - (2*i+1); j++){
+            System.out.print("*");
+        }
+
+        // spaces
+        for(int j = 0; j < i; j++){
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+   }
+public static void main(String[] args){
+    Pattern8 p = new  Pattern8();
+    p.pat8(5);
+}
+
+}
