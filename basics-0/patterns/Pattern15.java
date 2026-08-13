be3d043893fd9afed08c1b4a50e9f7d102230bb7
Abhishek Pandey <hopesaliveabhi2003@gmail.com>
2026-08-13 20:53:46 +0530

    inverted char triangle

---
+++ basics-0/patterns/Pattern15.java
@@ -0,0 +1,16 @@
+package patterns;
+
+public class Pattern15 {
+    public void pat15(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= (n+1)-i; j++){
+                System.out.print((char)(64 + j)+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        Pattern15 p = new Pattern15();
+        p.pat15(5);
+    }
+}
